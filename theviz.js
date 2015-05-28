d798e57af7b74d187e1821c7ee92ed818576c4f6
jksarathy <jayshree.sarathy@yale.edu>
2015-05-28 15:01:51 -0700

    Update theviz.js

--- theviz.js
+++ theviz.js
@@ -28,7 +28,7 @@ var sparkCountry ="CHN";
 var map;
 
 $("edit-select-countries").select2( {
-	maximumSelectionSize: 3
+	maximumSelectionSize: 3,
 	placeholder: "Select a state"
 });
 $("#submit_button").onclick = function () {
