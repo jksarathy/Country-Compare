7651451cfd547d83a26762aa785879800b6bae9e
Jayshree Sarathy <jksarathy@gmail.com>
2015-06-08 14:01:54 -0700

    trial with rich chart

--- theviz.js
+++ theviz.js
@@ -169,7 +169,8 @@ function addTable(i, country) {
 }
 
 function runCharts() {
-	drawMap();
+	drawRichMap;
+	//drawMap();
 	//drawSpark(sparkCountry);
 	initLine();
 	$.each(countryselection, function(i, d) {
@@ -310,6 +311,155 @@ function drawGauge(key, country) {
     };
     return empty;
 }*/
+
+function drawRichMap() {
+
+    // For each country, use the latest value for current population
+    var data = [];
+
+    // Add lower case codes to the data set for inclusion in the tooltip.pointFormat
+    var mapData = Highcharts.geojson(Highcharts.maps['countries/co/co-all']);
+    $.each(mapData, function () {
+        this.id = this.properties['hc-key']; // for Chart.get()
+    });
+
+    // Wrap point.select to get to the total selected points
+    Highcharts.wrap(Highcharts.Point.prototype, 'select', function (proceed) {
+
+    	proceed.apply(this, Array.prototype.slice.call(arguments, 1));
+
+    	var points = mapChart.getSelectedPoints();
+
+    	if (points.length) {
+    		if (points.length === 1) {
+    			$('#info h2').html(points[0].name);
+    		} else {
+    			$('#info h2').html('Comparing countries');
+
+    		}
+    		$('#info .subheader').html('<h4>Historical population</h4><small><em>Shift + Click on map to compare countries</em></small>')
+
+    	/*	if (!countryChart) {
+    			countryChart = $('#country-chart').highcharts({
+    				chart: {
+    					height: 250,
+    					spacingLeft: 0
+    				},
+    				credits: {
+    					enabled: false
+    				},
+    				title: {
+    					text: null
+    				},
+    				subtitle: {
+    					text: null
+    				},
+    				xAxis: {
+    					tickPixelInterval: 50,
+    					crosshair: true
+    				},
+    				yAxis: {
+    					title: null,
+    					opposite: true
+    				},
+    				tooltip: {
+    					shared: true
+    				},
+    				plotOptions: {
+    					series: {
+    						animation: {
+    							duration: 500
+    						},
+    						marker: {
+    							enabled: false
+    						},
+    						threshold: 0,
+    						pointStart: parseInt(categories[0]),
+    					}
+    				}
+    			}).highcharts();
+    		}
+
+    		$.each(points, function (i) {
+                // Update
+                console.log(countries[this.code3].data);
+                if (countryChart.series[i]) {
+                    /*$.each(countries[this.code3].data, function (pointI, value) {
+                            countryChart.series[i].points[pointI].update(value, false);
+                        });
+    		countryChart.series[i].update({
+    			name: this.name,
+    			data: countries[this.code3].data,
+    			type: points.length > 1 ? 'line' : 'area'
+    		}, false);
+    	} else {
+    		countryChart.addSeries({
+    			name: this.name,
+    			data: countries[this.code3].data,
+    			type: points.length > 1 ? 'line' : 'area'
+    		}, false);
+    	}
+    }); 
+    		while (countryChart.series.length > points.length) {
+    			countryChart.series[countryChart.series.length - 1].remove(false);
+    		}
+    		countryChart.redraw();
+
+    	} else {
+    		$('#info h2').html('');
+    		$('#info .subheader').html('');
+    		if (countryChart) {
+    			countryChart = countryChart.destroy();
+    		}
+    	}
+    }); */
+    // Initiate the map chart
+    console.log(data, mapData);
+    mapChart = $('#map').highcharts('Map', {
+
+    	title: {
+    		text: null
+    	},
+
+    	subtitle: {
+    		text: null //'Source: <a href="http://data.worldbank.org/indicator/SP.POP.TOTL/countries/1W?display=default">The World Bank</a>'
+    	},
+
+    	mapNavigation: {
+    		enabled: true,
+    		buttonOptions: {
+    			verticalAlign: 'bottom'
+    		}
+    	},
+
+    	colorAxis: {
+    		type: 'logarithmic',
+    		endOnTick: false,
+    		startOnTick: false,
+    		min: 1000
+    	},
+
+    	tooltip: {
+    		footerFormat: '<span style="font-size: 10px">(Click for details)</span>'
+    	},
+
+    	series: [{
+    		data: data,
+    		mapData: mapData,
+    		joinBy: ['id', 'code3'],
+    		name: 'EPI ranking',
+    		allowPointSelect: true,
+    		cursor: 'pointer',
+    		states: {
+    			select: {
+    				color: '#a4edba',
+    				borderColor: 'black',
+    				dashStyle: 'shortdot'
+    			}
+    		}
+    	}]
+    }).highcharts();
+}
 
 function drawMap(){
 	var data = [];
